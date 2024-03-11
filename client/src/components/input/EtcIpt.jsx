4ae629b03d1ef17df96010455851424b1500a744
dongmay <sdh4202@naver.com>
2024-03-11 22:41:48 +0900

    Feat: join.jsx,login.jsx 작업중

---
+++ client/src/components/input/EtcIpt.jsx
@@ -0,0 +1,89 @@
+import styled from 'styled-components';
+import { BasicIpt } from './BasicIpt';
+import { BasicBtn } from '../button/BasicBtn';
+
+const LabelIpt = styled.label`
+  color: #777;
+  font-size: 15px;
+  font-weight: 400;
+  ${BasicIpt} {
+    margin-bottom: 15px;
+  }
+`;
+const IconIpt = styled.div`
+  position: relative;
+  ${BasicIpt} {
+    padding-left: 50px !important;
+  }
+  i {
+    position: absolute;
+    left: 20px;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+`;
+const BtnIpt = styled.form`
+  width: 100%;
+  height: 80px;
+  display: flex;
+  flex-wrap: nowrap;
+  gap: 10px;
+  padding: 10px 0px;
+  overflow: hidden;
+  ${BasicIpt} {
+    height: 100%;
+    border-radius: 100px;
+    border: 1px solid #000;
+  }
+  ${BasicBtn} {
+    flex-shrink: 0;
+    width: 80px;
+    font-size: 18px;
+    height: 100%;
+  }
+`;
+const SearchIpt = styled.form`
+  position: relative;
+  width: 100%;
+  height: 50px;
+  ${IconIpt} {
+    position: relative;
+    height: 100%;
+    width: 100%;
+    ${BasicIpt} {
+      font-size: 15px;
+      padding: 0 110px 0 50px;
+      height: 100%;
+      border: 1px solid #000;
+    }
+  }
+  ${BasicBtn} {
+    position: absolute;
+    right: 0;
+    top: 0;
+    width: 100px;
+    font-size: 15px;
+    height: 100%;
+    color: #333;
+    background: none;
+    border-radius: 0 10px 10px 0;
+    transition: all 0.3s;
+    box-shadow: none;
+    &::before {
+      content: '';
+      position: absolute;
+      left: 0;
+      top: 50%;
+      transform: translateY(-50%);
+      width: 1px;
+      height: 30px;
+      background-color: #888;
+    }
+    &:hover {
+      color: #fff;
+      background-color: #000;
+    }
+  }
+`;
+
+export { LabelIpt, IconIpt, BtnIpt, SearchIpt };
