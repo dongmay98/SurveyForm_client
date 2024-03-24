fe57178056f938fb4679c8d413a149c50493e740
dongmay98 <dongmay98@github.com>
2024-03-24 18:09:37 +0900

    Feat: store생성, Surveyform 에러수정

--- client-ts/src/pages/surveyform/Surveyform.tsx
+++ client-ts/src/pages/surveyform/Surveyform.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import styles from './css/SurveyStyle.module.css';
 
 type OptionType = {
@@ -8,24 +8,64 @@ type OptionType = {
 
 export default function SurveyForm() {
   const [options, setOptions] = useState<OptionType[]>([{ id: 1, text: '옵션1' }]);
+  const [mainElements, setMainElements] = useState<React.ReactElement[]>([]);
 
   const addOption = () => {
-    const newOptionId = options[options.length - 1].id + 1; // 이전 옵션 ID에 1을 더하여 새 ID 생성
+    const newOptionId = options[options.length - 1].id + 1;
     setOptions([...options, { id: newOptionId, text: `옵션${newOptionId}` }]);
   };
 
   const updateOptionText = (id: number, text: string) => {
-    setOptions(options.map(option => option.id === id ? { ...option, text } : option));
+    setOptions(options.map(option => (option.id === id ? { ...option, text } : option)));
   };
 
   const deleteOption = (id: number) => {
-    // 옵션이 하나만 남았을 때는 삭제하지 않음
     if (options.length > 1) {
       setOptions(options.filter(option => option.id !== id));
     } else {
-      // 옵션이 하나뿐일 때의 처리 로직
       alert("최소 한개이상의 옵션이 필요합니다.");
     }
+  };
+
+  const copyMainElement = () => {
+    const mainElement = (
+      <li className={styles.mainList}>
+        <div className={styles.titleSelect}>
+          <input type="text" placeholder="질문 제목" />
+          <select className={styles.case}>
+            <option value="단답형">단답형</option>
+            <option value="객관식">객관식</option>
+            <option value="체크박스">체크박스</option>
+          </select>
+        </div>
+        <div className={styles.optionContainer}>
+          <ul>
+            {options.map(option => (
+              <li key={option.id}>
+                <input
+                  className={styles.option}
+                  type="text"
+                  value={option.text}
+                  onChange={e => updateOptionText(option.id, e.target.value)}
+                />
+                <button className={styles.deleteOption} onClick={() => deleteOption(option.id)}>
+                  X
+                </button>
+              </li>
+            ))}
+          </ul>
+          <button className={styles.addOption} onClick={addOption} type="button">
+            옵션 추가
+          </button>
+        </div>
+        <div className={styles.copyPasteContainer}>
+          <button onClick={copyMainElement}>복사</button>
+          <button>삭제</button>
+        </div>
+      </li>
+    );
+
+    setMainElements([...mainElements, React.cloneElement(mainElement)]);
   };
 
   return (
@@ -53,21 +93,25 @@ export default function SurveyForm() {
                       className={styles.option}
                       type="text"
                       value={option.text}
-                      onChange={(e) => updateOptionText(option.id, e.target.value)}
+                      onChange={e => updateOptionText(option.id, e.target.value)}
                     />
-                    <button className={styles.deleteOption} 
-                    onClick={() => deleteOption(option.id)}>X</button>
+                    <button className={styles.deleteOption} onClick={() => deleteOption(option.id)}>
+                      X
+                    </button>
                   </li>
                 ))}
               </ul>
-              <button className={styles.addOption} onClick={addOption} type="button">옵션 추가</button>
+              <button className={styles.addOption} onClick={addOption} type="button">
+                옵션 추가
+              </button>
             </div>
             <div className={styles.copyPasteContainer}>
-              <button >복사</button>
-              <button >삭제</button>
+              <button onClick={copyMainElement}>복사</button>
+              <button>삭제</button>
             </div>
           </li>
         </ul>
+        {mainElements.map((element, index) => React.cloneElement(element, { key: index }))}
       </main>
     </section>
   );
