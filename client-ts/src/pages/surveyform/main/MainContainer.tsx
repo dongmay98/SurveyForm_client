158a57af9d31a64003bcf62c5d4fcbc3ad109e14
dongmay98 <sdh4202@naver.com>
2024-04-02 17:32:41 +0900

    test

--- client-ts/src/pages/surveyform/main/MainContainer.tsx
+++ client-ts/src/pages/surveyform/main/MainContainer.tsx
@@ -1,34 +1,77 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { RootState } from '../../../store/index';
-import {ActionButton,AddOptionButton,CaseSelect,CopyPasteContainer,DeleteOptionButton,Main,MainList,OptionContainer,OptionInput,TitleInput,TitleSelect} from '../../../components/main/mainSurvey'
-import QuestionTypeSelect from '../QuestionTypeSelect/QuestionTypeSelect';
-import { QUESTION_TYPE } from 'QuestionType';
-import ShortOption from '../ShortOption/ShortOption';
-import MultipleOption from '../MultipleOption/MultipleOption';
-import CheckboxOption from '../CheckboxOption/CheckboxOption';
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../../store/index";
+import {
+  ActionButton,
+  AddOptionButton,
+  CaseSelect,
+  CopyPasteContainer,
+  DeleteOptionButton,
+  Main,
+  MainList,
+  OptionContainer,
+  OptionInput,
+  TitleInput,
+  TitleSelect,
+} from "../../../components/main/mainSurvey";
+import { setQuestionTitle } from "../../../store/surveySlice";
+import QuestionTypeSelect from "../QuestionTypeSelect/QuestionTypeSelect";
+import { QUESTION_TYPE } from "QuestionType";
+import ShortOption from "../ShortOption/ShortOption";
+import MultipleOption from "../MultipleOption/MultipleOption";
+import CheckboxOption from "../CheckboxOption/CheckboxOption";
 
 export default function MainContainer() {
   const dispatch = useDispatch();
   const survey = useSelector((state: RootState) => state.survey);
   const { questions } = survey;
-  const currentQuestion = questions[0];
-
+  const handleTitleChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    index: number
+  ) => {
+    dispatch(
+      setQuestionTitle({
+        questionIndex: index,
+        questionTitle: e.target.value,
+      })
+    );
+  };
 
   return (
-      <Main>
-      <MainList>
-        <TitleSelect>
-          <TitleInput type="text" placeholder="질문 제목" value={currentQuestion.questionTitle} />
-          <QuestionTypeSelect questionIndex={0} />
-        </TitleSelect>
-        {currentQuestion.type === QUESTION_TYPE.SHORT && <ShortOption questionIndex={0} />}
-        {currentQuestion.type === QUESTION_TYPE.MULTIPLE && <MultipleOption questionIndex={0} />}
-        {currentQuestion.type === QUESTION_TYPE.CHECKBOX && <CheckboxOption questionIndex={0} />}
-        <CopyPasteContainer>
-          <ActionButton>복사</ActionButton>
-          <ActionButton>삭제</ActionButton>
-        </CopyPasteContainer>
-      </MainList>
-    </Main>
+    <>
+      {questions.map((_, index) => {
+        return (
+          <Main>
+            <MainList>
+              <TitleSelect>
+                <TitleInput
+                  type="text"
+                  placeholder="질문 제목"
+                  value={questions[index].questionTitle}
+                  onChange={(e) => handleTitleChange(e, index)}
+                />
+                <QuestionTypeSelect questionIndex={index} />
+              </TitleSelect>
+              {(() => {
+                switch (questions[index].type) {
+                  case QUESTION_TYPE.SHORT:
+                    return <ShortOption questionIndex={index} />;
+                  case QUESTION_TYPE.MULTIPLE:
+                    return <MultipleOption questionIndex={index} />;
+                  case QUESTION_TYPE.CHECKBOX:
+                    return <CheckboxOption questionIndex={index} />;
+
+                  default:
+                    return null;
+                }
+              })()}
+              <CopyPasteContainer>
+                <ActionButton>복사</ActionButton>
+                <ActionButton>삭제</ActionButton>
+              </CopyPasteContainer>
+            </MainList>
+          </Main>
+        );
+      })}
+    </>
   );
 }
